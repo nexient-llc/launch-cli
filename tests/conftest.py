be7d85b22837b1c82ce6e1def696e871233aa545
Chris Taylor <chris11.taylor@nttdata.com>
2023-11-14 13:22:51 -0600

    Add fixtures for manipulating env vars during test runs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
-# Shared fixtures go here.
+import os
+
 import pytest
 from click import testing as click_testing
 
@@ -6,3 +7,21 @@ from click import testing as click_testing
 @pytest.fixture
 def cli_runner():
     yield click_testing.CliRunner()
+
+
+@pytest.fixture(scope="function")
+def no_github_token(set_environment_variables):
+    token_value = os.environ["GITHUB_TOKEN"]
+    del os.environ["GITHUB_TOKEN"]
+    yield
+    os.environ["GITHUB_TOKEN"] = token_value
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_environment_variables():
+    old_environment = dict(os.environ)
+    os.environ.clear()
+    os.environ.update({"GITHUB_TOKEN": "ghp_test_value"})
+    yield
+    os.environ.clear()
+    os.environ.update(old_environment)
