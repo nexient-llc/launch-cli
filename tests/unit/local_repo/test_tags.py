69279b5d12478ff4d6a49ca08bdf09e26f535bbd
Chris Taylor <chris11.taylor@nttdata.com>
2023-11-27 13:30:57 -0600

    Add fixture for ephemeral git repo in test context, add tests for new tag functions and command help verification

--- tests/unit/local_repo/test_tags.py
+++ tests/unit/local_repo/test_tags.py
@@ -2,29 +2,52 @@ import pytest
 from git.repo import Repo
 from semver import Version
 
-from launch.local_repo.tags import read_semantic_tags, read_tags
+from launch.local_repo import tags  # Used for mocking only
+from launch.local_repo.tags import (
+    acquire_repo,
+    create_version_tag,
+    push_version_tag,
+    read_semantic_tags,
+    read_tags,
+)
 
 
-def test_get_tags(tmp_path):
-    temp_repo = Repo.init(path=tmp_path)
-    tmp_path.joinpath("test.txt").write_text("Sample file")
-    temp_repo.index.add("test.txt")
-    temp_repo.index.commit("Added test.txt")
-    temp_repo.create_tag("0.1.0")
-    temp_repo.create_tag("not-semantic!")
-    tags = read_tags(repo_path=tmp_path)
+def test_acquire_repo(example_github_repo):
+    acquired_repo = acquire_repo(repo_path=example_github_repo.working_dir)
+    assert example_github_repo == acquired_repo
+
+
+def test_get_tags(example_github_repo):
+    example_github_repo.create_tag("not-semantic!")
+    tags = read_tags(repo_path=example_github_repo.working_dir)
     assert len(tags) == 2
     assert "0.1.0" in tags
     assert "not-semantic!" in tags
 
 
-def test_get_semantic_tags(tmp_path):
-    temp_repo = Repo.init(path=tmp_path)
-    tmp_path.joinpath("test.txt").write_text("Sample file")
-    temp_repo.index.add("test.txt")
-    temp_repo.index.commit("Added test.txt")
-    temp_repo.create_tag("0.1.0")
-    temp_repo.create_tag("not-semantic!")
-    tags = read_semantic_tags(repo_path=tmp_path)
+def test_get_semantic_tags(example_github_repo):
+    example_github_repo.create_tag("not-semantic!")
+    tags = read_semantic_tags(repo_path=example_github_repo.working_dir)
     assert len(tags) == 1
     assert Version.parse("0.1.0") in tags
+
+
+def test_create_version_tag(example_github_repo):
+    new_version = Version(major=1, minor=2, patch=3)
+    new_ref = create_version_tag(
+        repo_path=example_github_repo.working_dir, version=new_version
+    )
+    assert new_ref.name == "1.2.3"
+
+
+def test_push_version_tag(example_github_repo, mocker):
+    # Since we don't really have a remote to push to, mocking is about all we can do.
+    new_version = Version(major=1, minor=0, patch=0)
+    new_ref = create_version_tag(
+        repo_path=example_github_repo.working_dir, version=new_version
+    )
+    repo_mock = mocker.MagicMock()
+    mocker.patch.object(tags, "acquire_repo", return_value=repo_mock)
+    push_version_tag(repo_path=example_github_repo.working_dir, tag=new_ref)
+    repo_mock.remote.assert_called_with("origin")
+    repo_mock.remote.call_count == 2
--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -1,6 +1,7 @@
 from launch.cli.entrypoint import cli
 from launch.cli.github.access.commands import set_default
 from launch.cli.github.hooks.commands import create
+from launch.cli.github.version.commands import apply, predict
 
 
 def test_cli_help(cli_runner):
@@ -18,4 +19,16 @@ def test_github_access_command_help(cli_runner):
 def test_github_hooks_command_help(cli_runner):
     result = cli_runner.invoke(create, "--help")
     assert "create" in result.output
+    assert not result.exception
+
+
+def test_github_version_predict_help(cli_runner):
+    result = cli_runner.invoke(predict, "--help")
+    assert "predict" in result.output
+    assert not result.exception
+
+
+def test_github_version_apply_help(cli_runner):
+    result = cli_runner.invoke(apply, "--help")
+    assert "creates and pushes" in result.output
     assert not result.exception
