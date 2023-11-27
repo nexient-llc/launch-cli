b689c333a440edc0607e413cf07a5639e16e8c32
Chris Taylor <chris11.taylor@nttdata.com>
2023-11-27 13:27:19 -0600

    Implement semver apply

--- launch/cli/github/version/commands.py
+++ launch/cli/github/version/commands.py
@@ -3,8 +3,12 @@ import pathlib
 import click
 
 from launch.local_repo.branch import get_current_branch_name
-from launch.local_repo.predict import InvalidBranchNameException, predict_version
-from launch.local_repo.tags import read_semantic_tags
+from launch.local_repo.predict import predict_version
+from launch.local_repo.tags import (
+    create_version_tag,
+    push_version_tag,
+    read_semantic_tags,
+)
 
 
 @click.command()
@@ -22,7 +26,7 @@ from launch.local_repo.tags import read_semantic_tags
     help="Predict next version of the repository located at this path. Can be relative or absolute, defaults to the current directory.",
 )
 def predict(repo_path: pathlib.Path):
-    """Creates a webhook for a single repository."""
+    """Predicts the next semantic version for a repository."""
 
     try:
         active_branch = get_current_branch_name(repo_path=repo_path)
@@ -51,8 +55,43 @@ def predict(repo_path: pathlib.Path):
         path_type=pathlib.Path,
     ),
     default=".",
-    help="Predict next version of the repository located at this path. Can be relative or absolute, defaults to the current directory.",
+    help="Increment version of the repository located at this path. Can be relative or absolute, defaults to the current directory.",
 )
-def apply(repo_path: pathlib.Path):
-    """Creates a webhook for a single repository."""
-    raise NotImplementedError(f"would be applying version for repo at {repo_path}")
+@click.option(
+    "--source-branch",
+    type=click.STRING,
+    help="Name of the branch that should be used to predict the next semantic version.",
+)
+def apply(repo_path: pathlib.Path, source_branch: str):
+    """Predicts the next semantic version for a repository based on the provided source branch, then creates and pushes a tag."""
+    # Safeguard to ensure that we can't accidentally bump a version if the branch is being merged against anything but main.
+    active_branch = get_current_branch_name(repo_path=repo_path)
+    if not active_branch == "main":
+        click.secho(
+            f"Failed to apply next version for repository at {repo_path}: repo is not on main branch!",
+            fg="red",
+        )
+        raise click.Abort()
+
+    try:
+        predicted_version = predict_version(
+            existing_tags=read_semantic_tags(repo_path=repo_path),
+            branch_name=source_branch,
+        )
+    except Exception as e:
+        click.secho(
+            f"Failed to apply next version for repository at {repo_path} during prediction: {e}",
+            fg="red",
+        )
+        raise click.Abort()
+
+    try:
+        new_tag = create_version_tag(repo_path=repo_path, version=predicted_version)
+        push_version_tag(repo_path=repo_path, tag=new_tag)
+        click.echo(f"Version is now {predicted_version}")
+    except Exception as e:
+        click.secho(
+            f"Failed to apply next version for repository at {repo_path} during tagging: {e}",
+            fg="red",
+        )
+        raise click.Abort()
--- launch/local_repo/tags.py
+++ launch/local_repo/tags.py
@@ -1,17 +1,22 @@
 import pathlib
 from contextlib import suppress
 
+from git import TagReference
 from git.repo import Repo
 from semver import Version
 
 
-def read_tags(repo_path: pathlib.Path) -> list[str]:
+def acquire_repo(repo_path: pathlib.Path) -> Repo:
     try:
-        repo_instance = Repo(path=repo_path)
+        return Repo(path=repo_path)
     except Exception as e:
         raise RuntimeError(
             f"Failed to get a Repo instance from path {repo_path}: {e}"
         ) from e
+
+
+def read_tags(repo_path: pathlib.Path) -> list[str]:
+    repo_instance = acquire_repo(repo_path=repo_path)
     return [tag.name for tag in repo_instance.tags]
 
 
@@ -23,3 +28,15 @@ def read_semantic_tags(repo_path: pathlib.Path) -> list[Version]:
         with suppress(ValueError):
             semver_tags.append(Version.parse(tag))
     return semver_tags
+
+
+def create_version_tag(repo_path: pathlib.Path, version: Version) -> TagReference:
+    repo_instance = acquire_repo(repo_path=repo_path)
+    return repo_instance.create_tag(str(version))
+
+
+def push_version_tag(
+    repo_path: pathlib.Path, tag: TagReference, origin_name: str = "origin"
+):
+    repo_instance = acquire_repo(repo_path=repo_path)
+    repo_instance.remote(origin_name).push(tag.name)
