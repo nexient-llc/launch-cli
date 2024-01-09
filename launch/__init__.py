5c722d88ca376f3220bf66e1b3b1049175fdfe76
Chris Taylor <chris11.taylor@nttdata.com>
2024-01-09 08:01:57 -0600

    Add a --version flag to the entrypoint to print the current version of the tool

---
+++ launch/__init__.py
@@ -0,0 +1,4 @@
+from semver import Version
+
+VERSION = "0.1.0"
+SEMANTIC_VERSION = Version.parse(VERSION)
--- launch/cli/entrypoint.py
+++ launch/cli/entrypoint.py
@@ -1,9 +1,10 @@
 import logging
+import sys
 
 import click
 
 
-@click.group(name="cli")
+@click.group(name="cli", invoke_without_command=True)
 @click.option(
     "--verbose",
     "-v",
@@ -12,13 +13,25 @@ import click
     default=False,
     help="Increase verbosity of all subcommands",
 )
-def cli(verbose):
+@click.option(
+    "--version",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Prints the current version of the tool and immediately exits.",
+)
+def cli(verbose, version):
     """Launch CLI tooling to help automate common tasks performed by Launch engineers and their clients."""
     logging.basicConfig(
         level=logging.DEBUG if verbose else logging.INFO,
         format="%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s",
         datefmt="%F %T %Z",
     )
+    if version:
+        from launch import SEMANTIC_VERSION
+
+        logging.info(f"Launch CLI Version {SEMANTIC_VERSION}")
+        sys.exit(0)
 
 
 from .github import github_group
