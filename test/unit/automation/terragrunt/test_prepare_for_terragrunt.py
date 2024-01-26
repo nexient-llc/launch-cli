b60404ebc2b924ec4cb5e865198964c27b3aff5b
aaron-launch <aaron.christian@nttdata.com>
2024-01-26 20:03:39 +0000

    removing test file thats currently not being used

--- test/unit/automation/terragrunt/test_prepare_for_terragrunt.py
+++
@@ -1,4 +0,0 @@
-import git
-from unittest.mock import patch, MagicMock
-from launch.automation.terragrunt.functions import prepare_for_terragrunt
-from launch.automation.provider.aws.functions import assume_role
