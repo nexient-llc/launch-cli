1f6c2f73c7913eaec1afc14b0005cdc386b9643b
Ben Vaughan <ben.vaughan@nttdata.com>
2024-02-27 09:18:28 -0600

    feat: add helm commands * add helm command group * add helm command to recursively resolve helm chart dependencies in a given path

--- src/launch/automation/helm/functions.py
+++ src/launch/automation/helm/functions.py
@@ -16,33 +16,17 @@ def resolve_dependencies(helm_directory: pathlib.Path) -> None:
     Raises:
         FileNotFoundError: If no Chart.yaml is found in the provided directory.
     """
-    # Entrypoint for CLI logic
     top_level_chart = helm_directory.joinpath("Chart.yaml")
 
     if not top_level_chart.exists():
         raise FileNotFoundError(f"No Chart.yaml found in {helm_directory}")
 
     dependencies = extract_dependencies_from_chart(chart_file=top_level_chart)
-    for dependency in dependencies:
-        if dependency["repository"].startswith("file://"):
-            # Local dependency, no need to fetch
-            continue
-        else:
-            subprocess.call(
-                ["helm", "repo", "add", dependency["name"], dependency["repository"]]
-            )
+    add_dependency_repositories(dependencies)
 
     subprocess.call(["helm", "dep", "build", "."], cwd=helm_directory)
-    for dependency in dependencies:
-        # Discover the dependency archives -- there should be one for each version, but only ever one version included as a dependency.
-        dependency_archives = discover_files(
-            helm_directory.joinpath("charts"), filename_partial=dependency["name"]
-        )
-        for dependency_archive in dependency_archives:
-            unpack_archive(dependency_archive, helm_directory.joinpath("charts"))
-            resolve_dependencies(
-                helm_directory.joinpath(f"charts/{dependency['name']}")
-            )
+
+    resolve_next_layer_dependencies(dependencies, helm_directory)
 
 
 def extract_dependencies_from_chart(chart_file: pathlib.Path) -> list[dict[str, str]]:
@@ -58,3 +42,40 @@ def extract_dependencies_from_chart(chart_file: pathlib.Path) -> list[dict[str, 
     dependencies = yaml_contents.get("dependencies", [])
     logger.debug(f"Loaded {len(dependencies)} dependencies from chart at {chart_file}")
     return dependencies
+
+
+def add_dependency_repositories(dependencies: list[dict[str, str]]) -> None:
+    """Adds Helm repositories for each dependency in the provided list.
+
+    Args:
+        dependencies (list[dict[str, str]]): A list of dependency objects.
+    """
+    for dependency in dependencies:
+        if dependency["repository"].startswith("file://"):
+            # Local dependency, no need to fetch
+            continue
+        else:
+            subprocess.call(
+                ["helm", "repo", "add", dependency["name"], dependency["repository"]]
+            )
+
+
+def resolve_next_layer_dependencies(
+    dependencies: list[dict[str, str]], helm_directory: pathlib.Path
+) -> None:
+    """Inspect any dependencies of the provided dependencies and resolve them.
+
+    Args:
+        dependencies (list[dict[str, str]]): A list of dependency objects.
+        helm_directory (pathlib.Path): Directory containing a Chart.yaml file.
+    """
+    for dependency in dependencies:
+        # Discover the dependency archives -- there should be one for each version, but only ever one version included as a dependency.
+        dependency_archives = discover_files(
+            helm_directory.joinpath("charts"), filename_partial=dependency["name"]
+        )
+        for dependency_archive in dependency_archives:
+            unpack_archive(dependency_archive, helm_directory.joinpath("charts"))
+            resolve_dependencies(
+                helm_directory.joinpath(f"charts/{dependency['name']}")
+            )
--- test/unit/automation/helm/test_helm_functions.py
+++ test/unit/automation/helm/test_helm_functions.py
@@ -1,6 +1,15 @@
+import pathlib
+import subprocess
 from test.unit.automation.helm.fixtures import isolated_helm_chart_with_dependencies
 
-from src.launch.automation.helm.functions import extract_dependencies_from_chart
+import pytest
+
+from src.launch.automation.helm.functions import (
+    add_dependency_repositories,
+    extract_dependencies_from_chart,
+    resolve_dependencies,
+    resolve_next_layer_dependencies,
+)
 
 
 def test_extract_dependencies(isolated_helm_chart_with_dependencies):
@@ -11,3 +20,78 @@ def test_extract_dependencies(isolated_helm_chart_with_dependencies):
     assert extracted_dependencies[0]["name"] == "helm-library"
     assert extracted_dependencies[0]["version"] == "^2.0.0"
     assert extracted_dependencies[0]["repository"] == "file://../../charts/helm-library"
+
+
+def test_resolve_dependencies_raises_error_if_no_chart_yaml(tmp_path):
+    with pytest.raises(FileNotFoundError):
+        resolve_dependencies(helm_directory=tmp_path)
+
+
+def test_add_dependency_repositories(mocker):
+    mocker.patch("subprocess.call")
+    dependencies = [
+        {
+            "name": "foo",
+            "version": "1.0.0",
+            "repository": "https://foo.com/helm-repo/charts/",
+        }
+    ]
+    add_dependency_repositories(dependencies)
+    subprocess.call.assert_called_once_with(
+        ["helm", "repo", "add", "foo", "https://foo.com/helm-repo/charts/"]
+    )
+
+
+def test_resolve_next_layer_dependencies(mocker):
+    # Mock the necessary functions and objects
+    dependencies = [
+        {"name": "dependency1", "version": "1.0.0"},
+        {"name": "dependency2", "version": "2.0.0"},
+    ]
+    helm_directory = pathlib.Path("/path/to/helm/directory")
+    discover_files_mock = mocker.patch(
+        "src.launch.automation.helm.functions.discover_files"
+    )
+    unpack_archive_mock = mocker.patch(
+        "src.launch.automation.helm.functions.unpack_archive"
+    )
+    resolve_dependencies_mock = mocker.patch(
+        "src.launch.automation.helm.functions.resolve_dependencies"
+    )
+
+    # Mock the return values of the mocked functions
+    discover_files_mock.return_value = [
+        "/path/to/dependency1_archive.tar.gz",
+        "/path/to/dependency2_archive.tar.gz",
+    ]
+
+    # Call the function under test
+    resolve_next_layer_dependencies(dependencies, helm_directory)
+
+    # Assert the expected function calls
+    discover_files_mock.assert_has_calls(
+        [
+            mocker.call(
+                helm_directory.joinpath("charts"), filename_partial="dependency1"
+            ),
+            mocker.call(
+                helm_directory.joinpath("charts"), filename_partial="dependency2"
+            ),
+        ]
+    )
+    unpack_archive_mock.assert_has_calls(
+        [
+            mocker.call(
+                "/path/to/dependency1_archive.tar.gz", helm_directory.joinpath("charts")
+            ),
+            mocker.call(
+                "/path/to/dependency2_archive.tar.gz", helm_directory.joinpath("charts")
+            ),
+        ]
+    )
+    resolve_dependencies_mock.assert_has_calls(
+        [
+            mocker.call(helm_directory.joinpath("charts/dependency1")),
+            mocker.call(helm_directory.joinpath("charts/dependency2")),
+        ]
+    )
